bc49ae0d5af389ed0b30077a0e7ee27b07b25bb4
ebrahimbaig <95580309+ebrahimbaig@users.noreply.github.com>
2023-10-11 06:14:03 +0500

    widget extraction

--- lib/UI/screens/onboarding/required_onboarding.dart
+++ lib/UI/screens/onboarding/required_onboarding.dart
@@ -1,4 +1,5 @@
 import 'package:premedpk_mobile_app/export.dart';
+import 'package:premedpk_mobile_app/ui/screens/onboarding/widgets/curve_painter.dart';
 import 'package:premedpk_mobile_app/ui/screens/onboarding/widgets/radio_button.dart';
 import 'package:premedpk_mobile_app/utils/Data/citites_data.dart';
 import 'package:premedpk_mobile_app/utils/Data/country_code_data.dart';
@@ -178,25 +179,4 @@ class saadOnbaording extends StatelessWidget {
       ),
     );
   }
-}
-
-class CurvePainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = Colors.white // Color of the curved shape
-      ..style = PaintingStyle.fill;
-
-    final path = Path()
-      ..lineTo(0, size.height * 0.75) // Start at the bottom-left corner
-      ..quadraticBezierTo(size.width * 0.5, size.height, size.width,
-          size.height * 0.75) // Create a curve
-      ..lineTo(size.width, 0) // Finish at the top-right corner
-      ..close();
-
-    canvas.drawPath(path, paint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
---
+++ lib/UI/screens/onboarding/widgets/curve_painter.dart
@@ -0,0 +1,22 @@
+import 'package:premedpk_mobile_app/export.dart';
+
+class CurvePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.white // Color of the curved shape
+      ..style = PaintingStyle.fill;
+
+    final path = Path()
+      ..lineTo(0, size.height * 0.75) // Start at the bottom-left corner
+      ..quadraticBezierTo(size.width * 0.5, size.height, size.width,
+          size.height * 0.75) // Create a curve
+      ..lineTo(size.width, 0) // Finish at the top-right corner
+      ..close();
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
