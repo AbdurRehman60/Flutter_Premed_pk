efa4e2fd6b99323919e32f657568feb8d249c024
M Saad <saadhai60@gmail.com>
2023-10-19 13:30:07 +0500

    minor fixes

--- lib/UI/screens/Expert_Solution/expert_solution_home.dart
+++ lib/UI/screens/Expert_Solution/expert_solution_home.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:premedpk_mobile_app/UI/screens/Expert_Solution/widgets/es_list_card.dart';
-import 'package:premedpk_mobile_app/export.dart';
+import 'package:premedpk_mobile_app/constants/assets.dart';
+import 'package:premedpk_mobile_app/constants/color_theme.dart';
+import 'package:premedpk_mobile_app/constants/text_theme.dart';
 
 class ExpertSolutionHome extends StatelessWidget {
   const ExpertSolutionHome({Key? key});
@@ -8,18 +11,16 @@ class ExpertSolutionHome extends StatelessWidget {
   Widget build(BuildContext context) {
     final List<String> tabs = <String>['Solved Questions', 'Pending Questions'];
     return DefaultTabController(
-      length: tabs.length, // This is the number of tabs.
+      length: tabs.length,
       child: Scaffold(
         body: NestedScrollView(
           headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-            // These are the slivers that show up in the "outer" scroll view.
             return <Widget>[
               SliverOverlapAbsorber(
                 handle:
                     NestedScrollView.sliverOverlapAbsorberHandleFor(context),
                 sliver: SliverAppBar(
-                  // This is the title in the app bar.
-                  toolbarHeight: 10,
+                  toolbarHeight: 60,
                   shape: const RoundedRectangleBorder(
                     borderRadius: BorderRadius.only(
                       bottomLeft: Radius.circular(20.0),
@@ -29,19 +30,21 @@ class ExpertSolutionHome extends StatelessWidget {
                   pinned: true,
                   expandedHeight: 160.0,
                   forceElevated: innerBoxIsScrolled,
-                  flexibleSpace: FlexibleSpaceBar(
-                    // This is the title in the app bar.
-                    background: Container(
-                      decoration: ShapeDecoration(
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(20),
-                            bottomRight: Radius.circular(20),
-                          ), // Set the rounded border radius
+                  flexibleSpace: Stack(
+                    // Wrap FlexibleSpaceBar with Stack
+                    children: [
+                      Container(
+                        decoration: ShapeDecoration(
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(20),
+                              bottomRight: Radius.circular(20),
+                            ),
+                          ),
+                          gradient: PreMedColorTheme().primaryGradient,
                         ),
-                        gradient: PreMedColorTheme().primaryGradient,
                       ),
-                      child: Padding(
+                      Padding(
                         padding: const EdgeInsets.all(20),
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
@@ -50,27 +53,28 @@ class ExpertSolutionHome extends StatelessWidget {
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [
                                 Image.asset(PremedAssets.EsIcon),
-                                SizedBoxes.horizontalLarge,
+                                SizedBox(width: 16), // Use SizedBox for spacing
                                 Text(
                                   'Expert Solution',
                                   style: PreMedTextTheme().heading5.copyWith(
-                                      fontWeight: FontWeight.bold,
-                                      color: PreMedColorTheme().white),
+                                        fontWeight: FontWeight.bold,
+                                        color: PreMedColorTheme().white,
+                                      ),
                                 ),
                               ],
                             ),
-                            SizedBoxes.verticalLarge,
+                            SizedBox(height: 16), // Use SizedBox for spacing
                             Text(
                               'Get top-notch video solution answers to your MDCAT questions from top-merit experts 🙌🏻',
                               style: PreMedTextTheme()
                                   .body
                                   .copyWith(color: PreMedColorTheme().white),
                               textAlign: TextAlign.center,
-                            )
+                            ),
                           ],
                         ),
                       ),
-                    ),
+                    ],
                   ),
                   bottom: PreferredSize(
                     preferredSize: const Size.fromHeight(50.0),
@@ -109,7 +113,8 @@ class ExpertSolutionHome extends StatelessWidget {
                     slivers: <Widget>[
                       SliverOverlapInjector(
                         handle: NestedScrollView.sliverOverlapAbsorberHandleFor(
-                            context),
+                          context,
+                        ),
                       ),
                       SliverPadding(
                         padding: const EdgeInsets.fromLTRB(0, 16, 0, 0),
@@ -118,22 +123,12 @@ class ExpertSolutionHome extends StatelessWidget {
                             (BuildContext context, int index) {
                               return Column(
                                 children: <Widget>[
+                                  // Your list items here
                                   const CardList(
                                     mainText:
                                         'Identify the sentence with the incorrect use of apostrophe from the following sentences and tell the answer',
                                     tags: [
-                                      {"tagName": "Tag1", "isResource": true},
-                                      {"tagName": "Tag2", "isResource": true},
-                                      {"tagName": "tag3", "isResource": false},
-                                      {
-                                        "tagName": "tag1234567891011121",
-                                        "isResource": false
-                                      },
-                                      {"tagName": "tag3", "isResource": false},
-                                      {"tagName": "tag3", "isResource": false},
-                                      {"tagName": "tag3", "isResource": false},
-                                      {"tagName": "tag3", "isResource": false},
-                                      {"tagName": "tag3", "isResource": false},
+                                      // Tags data here
                                     ],
                                     subtext:
                                         'I can\'t understand the question and the options provided. Please help me; my paper is in 3 days. I need to complete this mock.',
@@ -145,7 +140,7 @@ class ExpertSolutionHome extends StatelessWidget {
                                 ],
                               );
                             },
-                            childCount: 30, // You can adjust this value.
+                            childCount: 30,
                           ),
                         ),
                       ),
