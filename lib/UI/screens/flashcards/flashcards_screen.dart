00bae246d3be17aaaa7fafa070597081a416bba9
M Saad <saadhai60@gmail.com>
2023-11-06 14:46:34 +0500

    minor fixes

--- lib/UI/screens/flashcards/flashcards_screen.dart
+++ lib/UI/screens/flashcards/flashcards_screen.dart
@@ -5,9 +5,78 @@ import 'package:premedpk_mobile_app/constants/sized_boxes.dart';
 import 'package:premedpk_mobile_app/constants/text_theme.dart';
 import 'package:premedpk_mobile_app/utils/Data/flashcard_screen_data.dart';
 
+// class FlashcardScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(
+//           'Flashcards',
+//           style: PreMedTextTheme().heading5.copyWith(
+//                 color: PreMedColorTheme().black,
+//               ),
+//         ),
+//         backgroundColor: PreMedColorTheme().white,
+//       ),
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.start,
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: [
+//           Padding(
+//             padding: const EdgeInsets.only(left: 20),
+//             child: Text(
+//               'Select Questions and start revising.',
+//               style: PreMedTextTheme().subtext.copyWith(
+//                   color: PreMedColorTheme().neutral400,
+//                   fontWeight: FontWeight.w400),
+//             ),
+//           ),
+//           Expanded(
+//             child: Padding(
+//               padding: const EdgeInsets.all(20.0),
+//               child: GridView.builder(
+//                 itemCount: gridData.length,
+//                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+//                   crossAxisCount: 2,
+//                   mainAxisSpacing: 20,
+//                   crossAxisSpacing: 20,
+//                   mainAxisExtent: 130,
+//                 ),
+//                 itemBuilder: (context, index) {
+//                   Color color = Color(int.parse(
+//                       '0xFF${gridData[index]['color']?.substring(1)}'));
+//                   return FlashcardItem(
+//                     image: gridData[index]['image'] ?? '',
+//                     text: gridData[index]['text'] ?? '',
+//                     page: gridData[index]['page'] ?? '',
+//                     subject: gridData[index]['subject'] ?? '',
+//                     color: color, // Include subject
+//                   );
+//                 },
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+import 'package:flutter/material.dart';
+import 'package:premedpk_mobile_app/UI/screens/flashcards/flashcards_home.dart';
+import 'package:premedpk_mobile_app/constants/color_theme.dart';
+import 'package:premedpk_mobile_app/constants/sized_boxes.dart';
+import 'package:premedpk_mobile_app/constants/text_theme.dart';
+import 'package:premedpk_mobile_app/utils/Data/flashcard_screen_data.dart';
+
+import '../../../models/flashcard_model.dart';
+import '../../../utils/Data/flashcard_data.dart';
+
 class FlashcardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final subjectFlashcards =
+        getSubjectFlashcards(); // Calculate flashcard count
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -40,15 +109,22 @@ class FlashcardScreen extends StatelessWidget {
                   crossAxisCount: 2,
                   mainAxisSpacing: 20,
                   crossAxisSpacing: 20,
-                  mainAxisExtent: 126,
+                  mainAxisExtent: 130,
                 ),
                 itemBuilder: (context, index) {
+                  Color color = Color(int.parse(
+                      '0xFF${gridData[index]['color']?.substring(1)}'));
+                  final subject = gridData[index]['subject'] ?? '';
+                  final flashcardCount =
+                      subjectFlashcards[subject]?.length ?? 0;
+                  final page =
+                      '$flashcardCount Questions'; // Update page based on count
                   return FlashcardItem(
                     image: gridData[index]['image'] ?? '',
                     text: gridData[index]['text'] ?? '',
-                    page: gridData[index]['page'] ?? '',
-                    subject:
-                        gridData[index]['subject'] ?? '', // Include subject
+                    page: page,
+                    subject: subject,
+                    color: color,
                   );
                 },
               ),
@@ -58,6 +134,26 @@ class FlashcardScreen extends StatelessWidget {
       ),
     );
   }
+
+  // This function calculates the flashcard count for each subject
+  Map<String, List<FlashcardModel>> getSubjectFlashcards() {
+    Map<String, List<FlashcardModel>> subjectFlashcards = {};
+
+    for (var data in gridData) {
+      String subject = data['subject']!;
+      if (!subjectFlashcards.containsKey(subject)) {
+        subjectFlashcards[subject] = [];
+      }
+      // Find flashcards for the subject
+      for (var flashcard in sampleFlashcards) {
+        if (flashcard.subject == subject) {
+          subjectFlashcards[subject]?.add(flashcard);
+        }
+      }
+    }
+
+    return subjectFlashcards;
+  }
 }
 
 class FlashcardItem extends StatelessWidget {
@@ -65,12 +161,14 @@ class FlashcardItem extends StatelessWidget {
   final String text;
   final String page;
   final String subject;
+  final Color color; // Use Color for background color
 
   FlashcardItem({
     required this.image,
     required this.text,
     required this.page,
-    required this.subject, // Add subject as a parameter
+    required this.subject,
+    required this.color, // Add color as a parameter
   });
 
   @override
@@ -90,10 +188,12 @@ class FlashcardItem extends StatelessWidget {
           // Handle the case where 'page' is empty or null.
         }
       },
-      child: Material(
-        shape: RoundedRectangleBorder(
+      child: Container(
+        // Use a Container to set the background color
+        decoration: BoxDecoration(
+          color: color, // Set the background color here
           borderRadius: BorderRadius.circular(12),
-          side: BorderSide(color: PreMedColorTheme().neutral300, width: 1),
+          border: Border.all(color: PreMedColorTheme().neutral300, width: 1),
         ),
         child: Padding(
           padding: const EdgeInsets.all(10.0),
@@ -121,7 +221,7 @@ class FlashcardItem extends StatelessWidget {
                 alignment: Alignment.bottomLeft,
                 child: Text(
                   page,
-                  style: PreMedTextTheme().heading7,
+                  style: PreMedTextTheme().subtext,
                 ),
               ),
             ],
--- lib/utils/Data/flashcard_screen_data.dart
+++ lib/utils/Data/flashcard_screen_data.dart
@@ -1,33 +1,40 @@
+import 'package:premedpk_mobile_app/export.dart';
+
 List<Map<String, String>> gridData = [
   {
     "image": "assets/images/biology.png",
     "text": "Biology",
     "page": "page1",
-    "subject": "Biology"
+    "subject": "Biology",
+    'color': '#EEE7F9',
   },
   {
     "image": "assets/images/chemistry.png",
     "text": "Chemistry",
     "page": "page2",
-    "subject": "Chemistry"
+    "subject": "Chemistry",
+    'color': '#FFF2D9'
   },
   {
     "image": "assets/images/physics.png",
     "text": "Physics",
     "page": "page3",
-    "subject": "Physics"
+    "subject": "Physics",
+    'color': '#FFEEED'
   },
   {
     "image": "assets/images/maths.png",
     "text": "Mathematics Reasoning",
     "page": "page3",
-    "subject": "Mathematics Reasoning"
+    "subject": "Mathematics Reasoning",
+    'color': '#E0F1FF'
   },
   {
     "image": "assets/images/science.png",
     "text": "Science Reasoning",
     "page": "page3",
-    "subject": "Science Reasoning"
+    "subject": "Science Reasoning",
+    'color': '#ECFBDB'
   },
   // Add more items as needed.
 ];
