0e2a12e214635b568744263926fbfaa0aecb4f6e
ebrahimbaig <baig.ebrahim@gmail.com>
2023-11-21 20:13:23 +0500

    bottomsheet added

--- lib/UI/screens/marketplace/cart.dart
+++ lib/UI/screens/marketplace/cart.dart
@@ -29,8 +29,8 @@ class Cart extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            CountdownTimerWidget(),
-            SizedBoxes.verticalMedium,
+            // CountdownTimerWidget(),
+            // SizedBoxes.verticalMedium,
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Column(
@@ -66,27 +66,33 @@ class Cart extends StatelessWidget {
                         itemBuilder: (context, index) {
                           BundleModel bundle =
                               cartProvider.selectedBundles[index];
-                          return Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Expanded(
-                                  child: CardContent(
-                                    bundle: bundle,
-                                    renderPoints: true,
-                                    renderDescription: false,
+                          return index != cartProvider.selectedBundles.length
+                              ? Padding(
+                                  padding: const EdgeInsets.all(16.0),
+                                  child: Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Expanded(
+                                        child: CardContent(
+                                          bundle: bundle,
+                                          renderPoints: true,
+                                          renderDescription: false,
+                                        ),
+                                      ),
+                                      IconButton(
+                                        icon: const Icon(Icons.close_rounded),
+                                        onPressed: () {
+                                          cartprovider.removeFromCart(bundle);
+                                        },
+                                      ),
+                                    ],
                                   ),
-                                ),
-                                IconButton(
-                                  icon: const Icon(Icons.close_rounded),
-                                  onPressed: () {
-                                    cartprovider.removeFromCart(bundle);
-                                  },
-                                ),
-                              ],
-                            ),
-                          );
+                                )
+                              : const SizedBox(
+                                  height: 800,
+                                  width: double.infinity,
+                                );
                         },
                       ),
                     ),
@@ -94,10 +100,151 @@ class Cart extends StatelessWidget {
                 ],
               ),
             ),
-            SizedBoxes.verticalMedium,
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
+            // SizedBoxes.verticalMedium,
+            // Padding(
+            //   padding: const EdgeInsets.symmetric(horizontal: 20),
+            //   child: Column(
+            //     crossAxisAlignment: CrossAxisAlignment.start,
+            //     children: [
+            //       Text(
+            //         'Coupon',
+            //         style: PreMedTextTheme().heading6.copyWith(
+            //               color: PreMedColorTheme().primaryColorRed,
+            //             ),
+            //       ),
+            //       SizedBoxes.verticalTiny,
+            //       Row(
+            //         mainAxisSize: MainAxisSize.min,
+            //         children: [
+            //           const Expanded(
+            //             flex: 3,
+            //             child: CustomTextField(
+            //               hintText: 'Enter Code',
+            //             ),
+            //           ),
+            //           SizedBoxes.horizontalTiny,
+            //           Expanded(
+            //             child: CustomButton(
+            //               buttonText: 'Apply',
+            //               onPressed: () {},
+            //             ),
+            //           )
+            //         ],
+            //       ),
+            //     ],
+            //   ),
+            // ),
+            // SizedBoxes.verticalMedium,
+            // Padding(
+            //   padding: const EdgeInsets.symmetric(horizontal: 20),
+            //   child: Container(
+            //     decoration: BoxDecoration(
+            //       color: PreMedColorTheme().white,
+            //       border: Border.all(
+            //         color: PreMedColorTheme().neutral300,
+            //         width: 2,
+            //       ),
+            //       borderRadius: BorderRadius.circular(10),
+            //     ),
+            //     width: double.infinity,
+            //     child: Padding(
+            //       padding: const EdgeInsets.all(16.0),
+            //       child: Column(
+            //         mainAxisAlignment: MainAxisAlignment.start,
+            //         crossAxisAlignment: CrossAxisAlignment.start,
+            //         children: [
+            //           Text(
+            //             'Cart Summary',
+            //             style: PreMedTextTheme().body.copyWith(
+            //                   fontWeight: FontWeight.bold,
+            //                 ),
+            //           ),
+            //           SizedBoxes.verticalTiny,
+            //           Text(
+            //             'Subtotal: Rs. ${cartProvider.totalOriginalPrice.toStringAsFixed(2)}',
+            //             style: PreMedTextTheme().body.copyWith(
+            //                   fontWeight: FontWeight.w500,
+            //                 ),
+            //           ),
+            //           SizedBoxes.verticalTiny,
+            //           Text(
+            //             'Discount (50% off) -Rs.${cartProvider.calculateTotalDiscount}',
+            //             style: PreMedTextTheme().body.copyWith(
+            //                   fontWeight: FontWeight.w500,
+            //                 ),
+            //           ),
+            //           SizedBoxes.verticalTiny,
+            //           Text(
+            //             'Coupon',
+            //             style: PreMedTextTheme().body.copyWith(
+            //                   fontWeight: FontWeight.w500,
+            //                 ),
+            //           ),
+            //           SizedBoxes.verticalTiny,
+            //           SizedBox(
+            //             width: 280,
+            //             child: Divider(
+            //               color: PreMedColorTheme().neutral200,
+            //             ),
+            //           ),
+            //           Text(
+            //             'Total',
+            //             style: PreMedTextTheme().heading5,
+            //           ),
+            //           Row(
+            //             children: [
+            //               Text('Rs. ${cartProvider.totalDiscountedPrice}',
+            //                   style: PreMedTextTheme().heading3.copyWith(
+            //                         color: PreMedColorTheme().primaryColorRed,
+            //                       )),
+            //               SizedBoxes.horizontalMedium,
+            //               Text(
+            //                 'Rs. ${cartProvider.totalOriginalPrice}',
+            //                 style: PreMedTextTheme().heading7.copyWith(
+            //                       color: PreMedColorTheme().neutral400,
+            //                       decoration: TextDecoration.lineThrough,
+            //                     ),
+            //               ),
+            //             ],
+            //           ),
+            //           SizedBoxes.verticalLarge,
+            //           CustomButton(
+            //             buttonText: 'Proceed to checkout ->',
+            //             onPressed: () {},
+            //           ),
+            //         ],
+            //       ),
+            //     ),
+            //   ),
+            // ),
+            // SizedBoxes.verticalMedium,
+          ],
+        ),
+      ),
+      bottomSheet: Container(
+        height: MediaQuery.of(context).size.height * 0.46,
+        decoration: BoxDecoration(
+          color: PreMedColorTheme().white,
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 10.0,
+              spreadRadius: 2.0,
+              offset: const Offset(0, -1), // Offset to give a top shadow effect
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
@@ -127,182 +274,72 @@ class Cart extends StatelessWidget {
                   ),
                 ],
               ),
-            ),
-            SizedBoxes.verticalMedium,
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: PreMedColorTheme().white,
-                  border: Border.all(
-                    color: PreMedColorTheme().neutral300,
-                    width: 2,
-                  ),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                width: double.infinity,
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Cart Summary',
-                        style: PreMedTextTheme().body.copyWith(
-                              fontWeight: FontWeight.bold,
-                            ),
-                      ),
-                      SizedBoxes.verticalTiny,
-                      Text(
-                        'Subtotal: Rs. ${cartProvider.totalOriginalPrice.toStringAsFixed(2)}',
-                        style: PreMedTextTheme().body.copyWith(
-                              fontWeight: FontWeight.w500,
-                            ),
-                      ),
-                      SizedBoxes.verticalTiny,
-                      Text(
-                        'Discount (50% off) -Rs.${cartProvider.calculateTotalDiscount}',
-                        style: PreMedTextTheme().body.copyWith(
-                              fontWeight: FontWeight.w500,
-                            ),
-                      ),
-                      SizedBoxes.verticalTiny,
-                      Text(
-                        'Coupon',
-                        style: PreMedTextTheme().body.copyWith(
-                              fontWeight: FontWeight.w500,
-                            ),
-                      ),
-                      SizedBoxes.verticalTiny,
-                      SizedBox(
-                        width: 280,
-                        child: Divider(
-                          color: PreMedColorTheme().neutral200,
-                        ),
-                      ),
-                      Text(
-                        'Total',
-                        style: PreMedTextTheme().heading5,
-                      ),
-                      Row(
-                        children: [
-                          Text('Rs. ${cartProvider.totalDiscountedPrice}',
-                              style: PreMedTextTheme().heading3.copyWith(
-                                    color: PreMedColorTheme().primaryColorRed,
-                                  )),
-                          SizedBoxes.horizontalMedium,
-                          Text(
-                            'Rs. ${cartProvider.totalOriginalPrice}',
-                            style: PreMedTextTheme().heading7.copyWith(
-                                  color: PreMedColorTheme().neutral400,
-                                  decoration: TextDecoration.lineThrough,
-                                ),
-                          ),
-                        ],
-                      ),
-                      SizedBoxes.verticalLarge,
-                      CustomButton(
-                        buttonText: 'Proceed to checkout ->',
-                        onPressed: () {},
-                      ),
-                    ],
-                  ),
+              SizedBoxes.verticalBig,
+              Text(
+                'Cart Summary',
+                style: PreMedTextTheme().body.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              SizedBoxes.verticalTiny,
+              Text(
+                'Subtotal: Rs. ${cartProvider.totalOriginalPrice.toStringAsFixed(2)}',
+                style: PreMedTextTheme().body.copyWith(
+                      fontWeight: FontWeight.w500,
+                    ),
+              ),
+              SizedBoxes.verticalTiny,
+              Text(
+                'Discount (50% off) -Rs.${cartProvider.calculateTotalDiscount}',
+                style: PreMedTextTheme().body.copyWith(
+                      fontWeight: FontWeight.w500,
+                    ),
+              ),
+              SizedBoxes.verticalTiny,
+              Text(
+                'Coupon',
+                style: PreMedTextTheme().body.copyWith(
+                      fontWeight: FontWeight.w500,
+                    ),
+              ),
+              SizedBoxes.verticalTiny,
+              SizedBox(
+                width: 280,
+                child: Divider(
+                  color: PreMedColorTheme().neutral200,
                 ),
               ),
-            ),
-            SizedBoxes.verticalMedium,
-          ],
+              Text(
+                'Total',
+                style: PreMedTextTheme().heading5,
+              ),
+              Row(
+                children: [
+                  Text(
+                    'Rs. ${cartProvider.totalDiscountedPrice}',
+                    style: PreMedTextTheme().heading3.copyWith(
+                          color: PreMedColorTheme().primaryColorRed,
+                        ),
+                  ),
+                  SizedBoxes.horizontalMedium,
+                  Text(
+                    'Rs. ${cartProvider.totalOriginalPrice}',
+                    style: PreMedTextTheme().heading7.copyWith(
+                          color: PreMedColorTheme().neutral400,
+                          decoration: TextDecoration.lineThrough,
+                        ),
+                  ),
+                ],
+              ),
+              SizedBoxes.verticalLarge,
+              CustomButton(
+                buttonText: 'Proceed to checkout ->',
+                onPressed: () {},
+              ),
+            ],
+          ),
         ),
       ),
-      // bottomSheet: Container(
-      //   height: MediaQuery.of(context).size.height * 0.35,
-      //   decoration: BoxDecoration(
-      //     color: PreMedColorTheme().white,
-      //     borderRadius: const BorderRadius.only(
-      //       topLeft: Radius.circular(20.0),
-      //       topRight: Radius.circular(20.0),
-      //     ),
-      //     boxShadow: [
-      //       BoxShadow(
-      //         color: Colors.black.withOpacity(0.1),
-      //         blurRadius: 10.0,
-      //         spreadRadius: 2.0,
-      //         offset: const Offset(0, -1), // Offset to give a top shadow effect
-      //       ),
-      //     ],
-      //   ),
-      //   child: Padding(
-      //     padding: const EdgeInsets.all(16.0),
-      //     child: Column(
-      //       mainAxisAlignment: MainAxisAlignment.start,
-      //       crossAxisAlignment: CrossAxisAlignment.start,
-      //       children: [
-      //         Text(
-      //           'Cart Summary',
-      //           style: PreMedTextTheme().body.copyWith(
-      //                 fontWeight: FontWeight.bold,
-      //               ),
-      //         ),
-      //         SizedBoxes.verticalTiny,
-      //         Text(
-      //           'Subtotal: Rs. ${cartProvider.totalOriginalPrice.toStringAsFixed(2)}',
-      //           style: PreMedTextTheme().body.copyWith(
-      //                 fontWeight: FontWeight.w500,
-      //               ),
-      //         ),
-      //         SizedBoxes.verticalTiny,
-      //         Text(
-      //           'Discount (50% off) -Rs.${cartProvider.calculateTotalDiscount}',
-      //           style: PreMedTextTheme().body.copyWith(
-      //                 fontWeight: FontWeight.w500,
-      //               ),
-      //         ),
-      //         SizedBoxes.verticalTiny,
-      //         Text(
-      //           'Coupon',
-      //           style: PreMedTextTheme().body.copyWith(
-      //                 fontWeight: FontWeight.w500,
-      //               ),
-      //         ),
-      //         SizedBoxes.verticalTiny,
-      //         SizedBox(
-      //           width: 280,
-      //           child: Divider(
-      //             color: PreMedColorTheme().neutral200,
-      //           ),
-      //         ),
-      //         Text(
-      //           'Total',
-      //           style: PreMedTextTheme().heading5,
-      //         ),
-      //         Row(
-      //           children: [
-      //             Text(
-      //               'Rs. ${cartProvider.totalDiscountedPrice}',
-      //               style: PreMedTextTheme().heading3.copyWith(
-      //                     color: PreMedColorTheme().primaryColorRed,
-      //                   ),
-      //             ),
-      //             SizedBoxes.horizontalMedium,
-      //             Text(
-      //               'Rs. ${cartProvider.totalOriginalPrice}',
-      //               style: PreMedTextTheme().heading7.copyWith(
-      //                     color: PreMedColorTheme().neutral400,
-      //                     decoration: TextDecoration.lineThrough,
-      //                   ),
-      //             ),
-      //           ],
-      //         ),
-      //         SizedBoxes.verticalLarge,
-      //         CustomButton(
-      //           buttonText: 'Proceed to checkout ->',
-      //           onPressed: () {},
-      //         ),
-      //       ],
-      //     ),
-      //   ),
-      // ),
     );
   }
 }
